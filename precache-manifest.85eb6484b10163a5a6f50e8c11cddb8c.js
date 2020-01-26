cc35f534a651b91868ab1e09a0c289ab25ca75ee
jeremy <jmra0611@gmail.com>
2020-01-25 23:21:29 -0400

    Updates

---
+++ precache-manifest.85eb6484b10163a5a6f50e8c11cddb8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9567f39a88901801112500dcdfa027a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d7d90c8a906d5c892b6",
+    "url": "/static/css/main.5a1509ae.chunk.css"
+  },
+  {
+    "revision": "8f2123057abdaeca3e5b",
+    "url": "/static/js/2.04b74a98.chunk.js"
+  },
+  {
+    "revision": "4ffedd1ed2f06696af6bcb88e4efc190",
+    "url": "/static/js/2.04b74a98.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0d7d90c8a906d5c892b6",
+    "url": "/static/js/main.f86e30f7.chunk.js"
+  },
+  {
+    "revision": "75a52bb0884f1c416f1b",
+    "url": "/static/js/runtime-main.a92b9e96.js"
+  }
+]);
